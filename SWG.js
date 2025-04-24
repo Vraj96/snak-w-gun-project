47371436ccbbd8b0cf708e192f9532d9a19caac0
Vraj96 <vrajg2044@gmail.com>
2025-04-24 17:12:46 +0530

    First commit

---
+++ SWG.js
@@ -0,0 +1,12 @@
+let user = prompt("Enter S,W or G");
+let cpuI = Math.floor(Math.random() *3);
+let cpu = ["S", "W", "G"][cpuI]
+
+const Match = (cpu, user) =>{
+if(cpu === user){
+    return 0 
+}else if (cpu==="S" & user==="W"){
+    
+}
+}
+Match(cpu, user)
